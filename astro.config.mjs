316661ee170c36527b3bb5f5233747d384397d9e
aryareal <aryadwiputra193@gmail.com>
2026-05-14 18:27:51 +0700

    dev: try deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 // @ts-check
 
 import svelte from "@astrojs/svelte";
-import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
 	integrations: [svelte()],
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
+	adapter: vercel(),
 	vite: {
 		plugins: [tailwindcss()],
 	},
